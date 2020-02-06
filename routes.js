a4a6b270064150f8b93206690a3cd499828b53e6
Song Cho <song@netrue.co.kr>
2020-02-06 11:06:39 +0900

    fix typo

--- routes.js
+++ routes.js
@@ -3,5 +3,8 @@ const { handler } = require("./handler");
 module.exports = app => {
   app.get("/", (req, res) => {
     handler("TRIGGERING PUSH");
-    res.send("Welcome to Push Notification Testing. REFRESH THE BROWSER TO TRIGGER PUSH");
+    res.send(
+      "Welcome to Push Notification Testing. REFRESH THE BROWSER TO TRIGGER PUSH"
+    );
+  });
 };
